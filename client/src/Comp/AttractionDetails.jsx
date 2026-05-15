68c3b61bc55ef157cf118f04ec2b1e5cbc315102
Hanaa-12 <151049887+Hanaa-12@users.noreply.github.com>
2026-05-15 11:53:00 +0400

    Update AttractionDetails.jsx

--- client/src/Comp/AttractionDetails.jsx
+++ client/src/Comp/AttractionDetails.jsx
@@ -376,7 +376,7 @@ export default function AttractionDetails() {
   >
      Delete
   </Button>
-</div>
+
                           </div>
                         )}
                       </CardBody>
